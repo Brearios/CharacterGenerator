b0fb741f513e8d9a0708d7bf3c8b90aad3ecf181
Aaron <presin16@gmail.com>
2019-09-29 20:01:09 -0500

    Added stat generation.

--- CharacterGenerator/Program.cs
+++ CharacterGenerator/Program.cs
@@ -11,6 +11,8 @@ namespace CharacterGenerator
             do
             { 
                 Generate();
+                Stats();
+                //GenerateStat(); Need to refactor stats to a method
                 Console.WriteLine("Press Y to generate another character, or any other key to exit.");
                 letter = Console.ReadLine();
             }
@@ -45,16 +47,16 @@ namespace CharacterGenerator
 
             int settingIndex = rand.Next(settingList.Length);
             string setting = settingList[settingIndex];
-            Console.WriteLine(setting);
+            //Console.WriteLine("This character is from " + setting);
 
             if (setting == "Prehistory")
             {
                 int nameIndex = rand.Next(preHistoryName.Length);
                 int tribeIndex = rand.Next(preHistoryTribe.Length);
                 int jobIndex = rand.Next(preHistoryJob.Length);
-                
-                Console.WriteLine("Welcome to pre-history, " + preHistoryName[nameIndex] + "!");
-                Console.WriteLine(preHistoryName[nameIndex] + " is a " + preHistoryJob[jobIndex] + " from the " + preHistoryTribe[tribeIndex] + " tribe.");
+
+                Console.WriteLine("Welcome to pre-historic tribalism, " + preHistoryName[nameIndex] + "!");
+                Console.WriteLine(preHistoryName[nameIndex] + " is a " + preHistoryJob[jobIndex] + " from the " + preHistoryTribe[tribeIndex] + " tribe.");               
             }
             else if (setting == "Fantasy")
             {
@@ -62,7 +64,7 @@ namespace CharacterGenerator
                 int jobIndex = rand.Next(fantasyJob.Length);
                 int placeIndex = rand.Next(fantasyPlace.Length);
 
-                Console.WriteLine("Go slay some dragons, " + fantasyName[fantasyNameIndex] + "!");
+                Console.WriteLine("Welcome to a world of fantasy " + fantasyName[fantasyNameIndex] + "! Now go slay some dragons!");
                 Console.WriteLine(fantasyName[fantasyNameIndex] + " is a " + fantasyJob[jobIndex] + " from " + fantasyPlace[placeIndex] + ".");
             }
             else if (setting == "Wild West")
@@ -72,7 +74,7 @@ namespace CharacterGenerator
                 int jobIndex = rand.Next(wildWestJob.Length);
                 int townIndex = rand.Next(wildWestPlace.Length);
 
-                Console.WriteLine("Welcome to the Wild West, " + wildWestFirstName[firstIndex] + " " + wildWestLastName[lastIndex] + "!");
+                Console.WriteLine("Welcome to the Wild West, " + wildWestFirstName[firstIndex] + " " + wildWestLastName[lastIndex] + " - watch out for rattlesnakes!");
                 Console.WriteLine(wildWestFirstName[firstIndex] + " is a " + wildWestJob[jobIndex] + " from " + wildWestPlace[townIndex] + ".");
             }
             else if (setting == "Present")
@@ -82,7 +84,7 @@ namespace CharacterGenerator
                 int jobIndex = rand.Next(job.Length);
                 int townIndex = rand.Next(town.Length);
 
-                Console.WriteLine("Welcome to the boring, normal world, " + firstName[firstIndex] + " " + lastName[lastIndex] + "!");
+                Console.WriteLine("Welcome to the ordinary, boring world, " + firstName[firstIndex] + " " + lastName[lastIndex] + "!");
                 Console.WriteLine(firstName[firstIndex] + " is a " + job[jobIndex] + " from " + town[townIndex] + ".");
             }
             else if (setting == "Post-Apocalypse")
@@ -91,7 +93,7 @@ namespace CharacterGenerator
                 int jobIndex = rand.Next(postApocJob.Length);
                 int townIndex = rand.Next(postApocLocations.Length);
 
-                Console.WriteLine("Welcome to what's left of the world, " + postApocName[firstIndex] + "!");
+                Console.WriteLine("Welcome," + postApocName[firstIndex] + " to what's left - the post-apocalypse!");
                 Console.WriteLine(postApocName[firstIndex] + " is a " + postApocJob[jobIndex] + " from " + postApocLocations[townIndex] + ".");
             }
             else if (setting == "Science Fiction")
@@ -100,7 +102,7 @@ namespace CharacterGenerator
                 int jobIndex = rand.Next(sciFiJob.Length);
                 int townIndex = rand.Next(sciFiPlaceName.Length);
 
-                Console.WriteLine("Welcome to the future, " + sciFiName[firstIndex] + "!");
+                Console.WriteLine("Welcome to the future, " + sciFiName[firstIndex] + "! Maybe *you'll* have a flying car.");
                 Console.WriteLine(sciFiName[firstIndex] + " is a " + sciFiJob[jobIndex] + " from " + sciFiPlaceName[townIndex] + ".");
             }
             else
@@ -108,13 +110,364 @@ namespace CharacterGenerator
                 Console.WriteLine("Failed to select a setting.");
             }
 
-            //int firstIndex = rand.Next(firstName.Length);
-            //int lastIndex = rand.Next(lastName.Length);
-            //int jobIndex = rand.Next(job.Length);
-            //int townIndex = rand.Next(town.Length);
+        }
+        static void Stats()
+        {
+            Random rand = new Random();
 
-            //Console.WriteLine("Welcome to the world, " + firstName[firstIndex] + " " + lastName[lastIndex] + "!");
-            //Console.WriteLine(firstName[firstIndex] + " is a " + job[jobIndex] + " from " + town[townIndex] + ".");
+            int intellectRoll = rand.Next(0, 100);
+            int strengthRoll = rand.Next(0, 100);
+            int agilityRoll = rand.Next(0, 100);
+            int staminaRoll = rand.Next(0, 100);
+            int energyRoll = rand.Next(0, 100);
+            int perceptionRoll = rand.Next(0, 100);
+            int luckRoll = rand.Next(0, 100);
+            int charismaRoll = rand.Next(0, 100);
+            int intellect;
+            int strength;
+            int agility;
+            int stamina;
+            int energy;
+            int perception;
+            int luck;
+            int charisma;
+
+            if (intellectRoll < 4)
+            {
+                intellect = 1;
+            }
+            else if (intellectRoll < 11)
+            {
+                intellect = 2;
+            }
+            else if (intellectRoll < 21)
+            {
+                intellect = 3;
+            }
+            else if (intellectRoll < 34)
+            {
+                intellect = 4;
+            }
+            else if (intellectRoll < 50)
+            {
+                intellect = 5;
+            }
+            else if (intellectRoll < 66)
+            {
+                intellect = 6;
+            }
+            else if (intellectRoll < 79)
+            {
+                intellect = 7;
+            }
+            else if (intellectRoll < 89)
+            {
+                intellect = 8;
+            }
+            else if (intellectRoll < 96)
+            {
+                intellect = 9;
+            }
+            else // > 96
+            {
+                intellect = 10;
+            }
+
+            if (strengthRoll < 4)
+            {
+                strength = 1;
+            }
+            else if (strengthRoll < 11)
+            {
+                strength = 2;
+            }
+            else if (strengthRoll < 21)
+            {
+                strength = 3;
+            }
+            else if (strengthRoll < 34)
+            {
+                strength = 4;
+            }
+            else if (strengthRoll < 50)
+            {
+                strength = 5;
+            }
+            else if (strengthRoll < 66)
+            {
+                strength = 6;
+            }
+            else if (strengthRoll < 79)
+            {
+                strength = 7;
+            }
+            else if (strengthRoll < 89)
+            {
+                strength = 8;
+            }
+            else if (strengthRoll < 96)
+            {
+                strength = 9;
+            }
+            else // > 96
+            {
+                strength = 10;
+            }
+
+            if (agilityRoll < 4)
+            {
+                agility = 1;
+            }
+            else if (agilityRoll < 11)
+            {
+                agility = 2;
+            }
+            else if (agilityRoll < 21)
+            {
+                agility = 3;
+            }
+            else if (agilityRoll < 34)
+            {
+                agility = 4;
+            }
+            else if (agilityRoll < 50)
+            {
+                agility = 5;
+            }
+            else if (agilityRoll < 66)
+            {
+                agility = 6;
+            }
+            else if (agilityRoll < 79)
+            {
+                agility = 7;
+            }
+            else if (agilityRoll < 89)
+            {
+                agility = 8;
+            }
+            else if (agilityRoll < 96)
+            {
+                agility = 9;
+            }
+            else // > 96
+            {
+                agility = 10;
+            }
+
+            if (staminaRoll < 4)
+            {
+                stamina = 60;
+            }
+            else if (staminaRoll < 11)
+            {
+                stamina = 70;
+            }
+            else if (staminaRoll < 21)
+            {
+                stamina = 80;
+            }
+            else if (staminaRoll < 34)
+            {
+                stamina = 90;
+            }
+            else if (staminaRoll < 50)
+            {
+                stamina = 100;
+            }
+            else if (staminaRoll < 66)
+            {
+                stamina = 110;
+            }
+            else if (staminaRoll < 79)
+            {
+                stamina = 120;
+            }
+            else if (staminaRoll < 89)
+            {
+                stamina = 130;
+            }
+            else if (staminaRoll < 96)
+            {
+                stamina = 140;
+            }
+            else // > 96
+            {
+                stamina = 150;
+            }
+
+            if (energyRoll < 4)
+            {
+                energy = 80;
+            }
+            else if (energyRoll < 11)
+            {
+                energy = 85;
+            }
+            else if (energyRoll < 21)
+            {
+                energy = 90;
+            }
+            else if (energyRoll < 34)
+            {
+                energy = 95;
+            }
+            else if (energyRoll < 50)
+            {
+                energy = 100;
+            }
+            else if (energyRoll < 66)
+            {
+                energy = 105;
+            }
+            else if (energyRoll < 79)
+            {
+                energy = 110;
+            }
+            else if (energyRoll < 89)
+            {
+                energy = 115;
+            }
+            else if (energyRoll < 96)
+            {
+                energy = 120;
+            }
+            else // > 96
+            {
+                energy = 125;
+            }
+
+            if (perceptionRoll < 4)
+            {
+                perception = 1;
+            }
+            else if (perceptionRoll < 11)
+            {
+                perception = 2;
+            }
+            else if (perceptionRoll < 21)
+            {
+                perception = 3;
+            }
+            else if (perceptionRoll < 34)
+            {
+                perception = 4;
+            }
+            else if (perceptionRoll < 50)
+            {
+                perception = 5;
+            }
+            else if (perceptionRoll < 66)
+            {
+                perception = 6;
+            }
+            else if (perceptionRoll < 79)
+            {
+                perception = 7;
+            }
+            else if (perceptionRoll < 89)
+            {
+                perception = 8;
+            }
+            else if (perceptionRoll < 96)
+            {
+                perception = 9;
+            }
+            else // > 96
+            {
+                perception = 10;
+            }
+
+            if (luckRoll < 4)
+            {
+                luck = 1;
+            }
+            else if (luckRoll < 11)
+            {
+                luck = 2;
+            }
+            else if (luckRoll < 21)
+            {
+                luck = 3;
+            }
+            else if (luckRoll < 34)
+            {
+                luck = 4;
+            }
+            else if (luckRoll < 50)
+            {
+                luck = 5;
+            }
+            else if (luckRoll < 66)
+            {
+                luck = 6;
+            }
+            else if (luckRoll < 79)
+            {
+                luck = 7;
+            }
+            else if (luckRoll < 89)
+            {
+                luck = 8;
+            }
+            else if (luckRoll < 96)
+            {
+                luck = 9;
+            }
+            else // > 96
+            {
+                luck = 10;
+            }
+
+            if (charismaRoll < 4)
+            {
+                charisma = 1;
+            }
+            else if (charismaRoll < 11)
+            {
+                charisma = 2;
+            }
+            else if (charismaRoll < 21)
+            {
+                charisma = 3;
+            }
+            else if (charismaRoll < 34)
+            {
+                charisma = 4;
+            }
+            else if (charismaRoll < 50)
+            {
+                charisma = 5;
+            }
+            else if (charismaRoll < 66)
+            {
+                charisma = 6;
+            }
+            else if (charismaRoll < 79)
+            {
+                charisma = 7;
+            }
+            else if (charismaRoll < 89)
+            {
+                charisma = 8;
+            }
+            else if (charismaRoll < 96)
+            {
+                charisma = 9;
+            }
+            else // > 96
+            {
+                charisma = 10;
+            }
+            Console.WriteLine("with the following attributes:");
+            Console.WriteLine("Intellect: " + intellect);
+            Console.WriteLine("Strength: " + strength);
+            Console.WriteLine("Agility: " + agility);
+            Console.WriteLine("Stamina: " + stamina);
+            Console.WriteLine("Energy: " + energy);
+            Console.WriteLine("Perception: " + perception);
+            Console.WriteLine("Luck: " + luck);
+            Console.WriteLine("Charisma: " + charisma);
         }
     }
 }
